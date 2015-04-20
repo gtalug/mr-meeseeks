e2daf32a78cc43b39f1b1d2d3024099da2c3fc3f
Alex Volkov <alex@flamy.ca>
2015-04-20 10:30:50 -0400

    PEP8 fixes.

--- meeseeks.py
+++ meeseeks.py
@@ -27,41 +27,49 @@ OPS_MEETING_DATE = list(dateutil_rrule.rrule(
     bysetpos=4
 ))[0]
 
+
 def send_email(subject, body):
     msg = MIMEText(body)
-    
+
     msg['Subject'] = subject
     msg['From'] = EMAIL_FROM
     msg['To'] = EMAIL_TO
-    
+
     s = smtplib.SMTP('localhost')
     s.sendmail(EMAIL_FROM, EMAIL_TO, msg.as_string())
     s.quit()
 
+
 def get_body(body):
     s = Template(body)
-    
+
     msg_body = s.substitute(
         meeting=MEETING_DATE.strftime(DATETIME_FORMAT),
         ops=OPS_MEETING_DATE.strftime(DATETIME_FORMAT)
     )
-    
+
     return msg_body
+
 
 def main(subject, body):
     msg_body = get_body(body)
-    
+
     send_email(subject, msg_body)
 
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.add_option('-f', '--file', dest='filename', help="Email template you wish to send.", metavar="FILE")
-    
+    parser.add_option(
+        '-f', '--file',
+        dest='filename',
+        help="Email template you wish to send.",
+        metavar="FILE"
+    )
+
     (options, args) = parser.parse_args()
-    
+
     subject = "I'm Mr. Meeseeks! Look at me!"
-    
+
     with open(options.filename, 'r') as f:
         body = f.read()
-    
+
     send_email(subject, body)
